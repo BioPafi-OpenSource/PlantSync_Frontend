753747bac39253a726302b75ee93d8f72c5f9100
Renzo <u20231d974@upc.edu.pe>
2025-05-14 03:29:54 -0500

    docs: add routing for chatbot
    added routing for chatbot page and a chatbot view template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { SidebarLayoutComponent } from './shared/components/sidebar-layout/sidebar-layout.component';
 import { plantsRoutes } from './features/plants/plants-routing.module';
 import {tasksRoutes} from "./features/tasks/tasks-routing.module";
-
+import {chatbotRoutes} from "./features/chatbot/chatbot-routing.module";
 export const routes: Routes = [
     {
         path: '',
@@ -16,7 +16,9 @@ export const routes: Routes = [
             {
                 path: 'tasks',
                 children: tasksRoutes
-            }
+            },
+            { path: 'chatbot',
+                children: chatbotRoutes }
         ]
     }
 ];
---
+++ src/app/features/chatbot/pages/chatbot-view/chatbot-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatbot-view',
+  imports: [],
+  templateUrl: './chatbot-view.component.html',
+  styleUrl: './chatbot-view.component.css'
+})
+export class ChatbotViewComponent {
+
+}
