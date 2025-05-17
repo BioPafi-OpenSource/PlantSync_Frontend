3470487b3d1aab3d337d48c7fcf141dc757db081
WoodsDos <javier.gonzales.2202@gmail.com>
2025-05-17 13:07:04 -0500

    undo

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+
+    BASE_URL: 'https://sustaining-halved-viburnum.glitch.me',
+    ENDPOINT_PATH_PLANTS: '/plants',
+    ENDPOINT_PATH_TASKS: '/tasks',
+    ENDPOINT_PATH_WEATHER_TIPS: '/weatherTips',
+    ENDPOINT_PATH_WEATHER_STATUS: '/weatherStatus',
+    ENDPOINT_PATH_GUIDES: '/guides',
+
+
+
+
+    RETRY_ATTEMPTS: 2
+
+};
