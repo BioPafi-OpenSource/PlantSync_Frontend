6a1be6ae035a082ead801f9039719af21fe7855c
WoodsDos <javier.gonzales.2202@gmail.com>
2025-07-07 03:07:15 -0500

    fix: fixed api key

--- src/app/features/chatbot/services/ai.service.ts
+++ src/app/features/chatbot/services/ai.service.ts
@@ -1,30 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AiService {
-  private apiUrl = 'https://api.openai.com/v1/chat/completions';
-  private apiKey = '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';
+  private apiUrl = 'https://api-key-ia.onrender.com/api/chat';
 
   constructor(private http: HttpClient) {}
 
   askQuestion(question: string): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${this.apiKey}`
-    });
-
-    const body = {
-      model: 'gpt-3.5-turbo',
-      messages: [
-        { role: 'system', content: 'Eres un asistente experto en plantas.' },
-        { role: 'user', content: question }
-      ]
-    };
-
-    return this.http.post(this.apiUrl, body, { headers });
+    return this.http.post(this.apiUrl, { question });
   }
 }
